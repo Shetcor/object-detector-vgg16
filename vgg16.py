2868096738d48db3bcde49652d365f750680f8c3
Shetcor <shetco15@gmail.com>
2021-06-20 21:55:44 +0200

    Changes

--- vgg16.py
+++ vgg16.py
@@ -105,3 +105,112 @@ label = label[0][0]
 # print the classification
 print('%s (%.2f%%)' % (label[1], label[2]*100))
 
+"""# Configuring streamlit"""
+
+!pip install -q streamlit
+
+!pip -q install pyngrok
+
+from pyngrok import ngrok
+
+!ngrok authtoken 1npJviy2rm2H1RzUbevBUv2GLGw_ep9y73PeZMfA5zeSBzzs
+
+# Commented out IPython magic to ensure Python compatibility.
+# %%writefile app.py
+# import streamlit as st
+# import tempfile
+# import cv2    
+# import math  
+# import pandas as pd
+# from keras.preprocessing.image import load_img
+# from keras.preprocessing.image import img_to_array
+# from keras.applications.vgg16 import preprocess_input
+# from keras.applications.vgg16 import decode_predictions
+# from keras.applications.vgg16 import VGG16
+# from PIL import Image
+# 
+# PAGE_CONFIG = {"page_title":"StColab.io", "page_icon":":smiley:","layout":"centered"}
+# st.set_page_config("PAGE_CONFIG")
+# 
+# st.title("VGG16 Object Detection")
+# st.sidebar.title("Dashboard")
+# def uploadFile():
+#   vid_file = st.file_uploader("Upload a video", type=["mp4", "mov","avi"])
+#   tempVideo = tempfile.NamedTemporaryFile(delete=False) 
+#   if vid_file is not None: 
+#     tempVideo.write(vid_file.read())
+#   return tempVideo.name
+#         
+# def splitVideo(videoPath):
+#   count = 0
+#   cap = cv2.VideoCapture(videoPath)
+#   frameRate = cap.get(5) 
+#   tempImage = tempfile.NamedTemporaryFile(delete=False) 
+#   x=1
+# 
+#   while(cap.isOpened()):
+#     frameId = cap.get(1) 
+#     ret, frame = cap.read()
+#     if (ret != True):
+#       break
+#     if (frameId % math.floor(frameRate) == 0):
+#       tempImage = videoPath.split('.')[0] +"_frame%d.jpg" % count;count+=1
+#       cv2.imwrite(tempImage, frame)
+#       frames.append(tempImage)
+#   cap.release() 
+#   return frames,count
+# 
+# def classifyObjects():  
+#   model = VGG16()
+#   classify = []
+#   frames,count = splitVideo(videoFile)
+# 
+#   for i in range(count):    
+#     image = load_img(frames[i], target_size=(224, 224)) 
+#     image = img_to_array(image)
+#     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
+#     image = preprocess_input(image)   
+#     img_pred = model.predict(image)
+#     label = decode_predictions(img_pred)    
+#     label = label[0][0]
+#     result =  label[1]
+#     classify.append(result)
+#   return classify
+# 
+# def searchInFrames(object_):
+#   indeces = []
+#   classifications = classifyObjects()
+#   if object_ in classifications:
+#     for i in range(len(classifications)):
+#       if classifications[i] == object_:
+#         index = classifications.index(object_)
+#         indeces.append(index)
+#         filePath = frames[index]
+#         img = load_img(filePath, target_size = (224, 224))
+#         detected_paths.append(filePath)
+#     for i in range(len(indeces)):
+#       st.image(frames[i], width=224)
+#   else:
+#     st.write("Object not available in video!")
+# 
+# videoFile = uploadFile()
+# user_input = st.text_input("Enter object to search: ")
+# 
+# if st.button('Search'):  
+#   frames =[]
+#   detected_paths = []
+#   searchInFrames(user_input)
+#   st.write("")
+
+!streamlit run --server.port 80 app.py &>/dev/null&
+
+address = ngrok.connect(port=8501)
+
+print(address)
+
+"""**If more processes run, kill tunnels**"""
+
+ngrok.kill()
+
+
+# %%
